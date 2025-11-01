b17ad472ffa19cb7986d34a1feaa182ba7868e77
rionakrosa <82613616+rionakrosa@users.noreply.github.com>
2025-11-01 13:26:46 +0100

    Create postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    // Tailwind's PostCSS plugin moved to a separate package
+    require('@tailwindcss/postcss'),
+    require('autoprefixer'),
+  ],
+}
+<body className="bg-transparent">
